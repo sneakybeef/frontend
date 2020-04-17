cee33146c5635a8f11e55f9c08f13bd0f5760ce3
john <luca.kebbel@yahoo.de>
2020-04-17 14:48:22 +0200

    refinement changing infra and working login

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { Swipeable } from "react-swipeable";
+import React from 'react';
+import { Swipeable } from 'react-swipeable';
 
 class Slider extends React.Component {
-  swiped = event => {
-    console.log(event);
-  };
+	swiped = (event) => {
+		console.log(event);
+	};
 
-  render() {
-    const config = {
-      onSwiped: event => this.swiped(event),
-      trackMouse: true
-    };
-    return <Swipeable {...config}>{this.props.children}</Swipeable>;
-  }
+	render() {
+		const config = {
+			onSwiped: (event) => this.swiped(event),
+			trackMouse: true
+		};
+		return <Swipeable {...config}>{this.props.children}</Swipeable>;
+	}
 }
 
 export default Slider;
